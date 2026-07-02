1bfb47ccc586b50e104b7644b005bcea4d5cc191
wiejskachatkapl-stack <wiejskachatka.pl@gmail.com>
2026-07-02 18:29:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,12 @@
-const CACHE_NAME = 'bingo-v1009-cache';
-const FILES = [
-  './',
-  './index.html?v=1009',
-  './style.css?v=1009',
-  './app.js?v=1009',
-  './manifest.json',
-  './assets/images/bingo_start.png',
-  './assets/images/bingo_game_bg.png',
-  './assets/icons/icon.svg'
-];
-
+// GAME ROOM v1108 - cache reset service worker
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)).catch(()=>{}));
   self.skipWaiting();
 });
-
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request).then(response => {
-      const copy = response.clone();
-      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy)).catch(()=>{});
-      return response;
-    }).catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
 });
